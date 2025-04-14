0b8174ccde391f12b8242e5654e0bbd7fad74b68
Mark Perkins <mark@allmarkedup.com>
2025-04-14 22:27:17 +0100

    Use Phlex #mix util for options merging

--- lib/slotify/services/tag_options_merger.rb
+++ lib/slotify/services/tag_options_merger.rb
@@ -1,53 +1,46 @@
 module Slotify
   module TagOptionsMerger
     class << self
-      include ::ActionView::Helpers::TagHelper
-
       def call(original, target)
-        original = original.to_h.deep_symbolize_keys
-        target = target.to_h.deep_symbolize_keys
-
-        target.each do |key, value|
-          original[key] = case key
-          when :data
-            merge_data_options(original[key], value)
-          when :class
-            merge_class_options(original[key], value)
-          else
-            value
-          end
-        end
-
-        original
+        mix(original, target)
       end
 
       private
 
-      def merge_data_options(original_data, target_data)
-        original_data = original_data.dup
+      # https://www.phlex.fun/sgml/helpers#mix
+      def mix(*args)
+        args.each_with_object({}) do |object, result|
+          result.merge!(object) do |_key, old, new|
+            case [old, new].freeze
+            in [Array, Array] | [Set, Set]
+              old + new
+            in [Array, Set]
+              old + new.to_a
+            in [Array, String]
+              old + [new]
+            in [Hash, Hash]
+              mix(old, new)
+            in [Set, Array]
+              old.to_a + new
+            in [Set, String]
+              old.to_a + [new]
+            in [String, Array]
+              [old] + new
+            in [String, Set]
+              [old] + new.to_a
+            in [String, String]
+              "#{old} #{new}"
+            in [_, nil]
+              old
+            else
+              new
+            end
+          end
 
-        target_data.each do |key, value|
-          values = [original_data[key], value]
-          original_data[key] = if key.in?([:controller, :action]) && all_kind_of?(String, values)
-            merge_strings(values)
-          else
-            value
+          result.transform_keys! do |key|
+            key.end_with?("!") ? key.name.chop.to_sym : key
           end
         end
-
-        original_data
-      end
-
-      def merge_class_options(original_classes, target_classes)
-        class_names(original_classes, target_classes)
-      end
-
-      def merge_strings(*args)
-        args.map(&:presence).compact.join(" ")
-      end
-
-      def all_kind_of?(kind, values)
-        values.none? { !_1.is_a?(kind) }
       end
     end
   end
