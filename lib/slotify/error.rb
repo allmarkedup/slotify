9a9d618e47f5987765b48ea12be92c74340ea5f9
Mark Perkins <mark@allmarkedup.com>
2025-04-16 01:19:09 +0100

    Remove unused errors classes

--- lib/slotify/error.rb
+++ lib/slotify/error.rb
@@ -2,16 +2,7 @@ module Slotify
   class UnknownSlotError < NameError
   end
 
-  class SlotsDefinedError < RuntimeError
-  end
-
-  class UndefinedSlotError < StandardError
-  end
-
   class MultipleSlotEntriesError < ArgumentError
-  end
-
-  class SlotArgumentError < ArgumentError
   end
 
   class StrictSlotsError < ArgumentError
